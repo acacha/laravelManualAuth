f94a68860cbb6ce1fd7e1f93c37ea9e61e9d81b3
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-10-04 19:09:57 +0200

    Sync

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,15 +6,27 @@ use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use PDO;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $user = User::find(1);
+        //Passos controlador bàsic (glue/cola del model i vista):
+        // 1) Aconseguir informació de l'usuari de la base de dades
+        // 2) Mostrar vista home passant info del usuari
+
+//        $user = User::find(1);
 
 //        $user->name = "asdasd";
+//        $pdo = new PDO('sqlite:/home/sergi/Code/laravelManualAuth/database/database.sqlite');
+//        $query = $pdo->prepare('SELECT * FROM users WHERE id=1');
+//        $query->execute();
+//        $row = $query->fetch();
+//        dd($row);
 
+        $user = new \stdClass();
+        $user->name = "Sergi Tur";
         return view('home')
             ->withUser($user);
 
--- config/app.php
+++ config/app.php
@@ -178,6 +178,15 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Acacha Llum Service Providers...
+         *
+         * See: https://github.com/acacha/llum
+         */
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+        #llum_providers
+
     ],
 
     /*
@@ -225,6 +234,14 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        /*
+         * Acacha Llum Aliases...
+         *
+         * See: https://github.com/acacha/llum
+         */
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        #llum_aliases
 
     ],
 
